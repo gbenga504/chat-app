2b6945be7f87f80fd56d0b1f019ae5465bb2e0d0
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2021-06-22 01:04:09 +0100

    Integrate the App with apis

--- src/App.js
+++ src/App.js
@@ -1,32 +1,80 @@
-import { memo, useRef, useEffect, useCallback } from 'react';
+import { memo, useRef, useEffect, useCallback, useMemo } from 'react';
 import { Box, makeStyles } from '@material-ui/core';
+import { useInfiniteQuery, useMutation, useQueryClient } from 'react-query';
 
 import ChatBackgroundImage from 'assets/chat_background.png';
 import ComposeBox from 'components/ComposeBox';
 import MaxWidthContainer from 'components/MaxWidthContainer';
 import MessageList from 'components/MessageList';
-import { MESSAGES } from 'mockData';
+import MessageService from 'services/MessageService';
+import { resources } from 'services/resources';
+import { appConfig } from 'config';
+import {
+  formatMessagesResponse,
+  mergeMessagePages,
+  performOptimisticUpdate,
+} from 'util/messageUtil';
 
 const App = () => {
   const classes = useStyles();
   const containerRef = useRef();
+  const queryClient = useQueryClient();
 
-  const handleScrollNotification = useCallback(() => {
-    if (containerRef.current.scrollTop === 0) {
-      //@todo: fetch more
+  const { data, isFetching, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    resources.MESSAGES,
+    ({ pageParam = 0 }) =>
+      MessageService.getAllMessages({ token: appConfig.apiToken, since: pageParam, limit: 10 }),
+    {
+      getNextPageParam: (lastPage) => lastPage?.[lastPage.length - 1]?.timestamp ?? false,
+      select: (data) => mergeMessagePages(data?.pages),
+    },
+  );
+
+  const { mutate: createMessage } = useMutation(MessageService.createMessage, {
+    onMutate: (payload) => performOptimisticUpdate(queryClient, payload),
+  });
+
+  const messages = useMemo(() => {
+    if (data) {
+      return formatMessagesResponse(data);
     }
-  }, []);
+    return [];
+  }, [data]);
+
+  const fetchPreviousPageOnScroll = useCallback(() => {
+    if (containerRef.current.scrollTop === 0 && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [hasNextPage, isFetching, fetchNextPage]);
 
   useEffect(() => {
     if (containerRef.current) {
-      containerRef.current.addEventListener('scroll', handleScrollNotification);
+      containerRef.current.addEventListener('scroll', fetchPreviousPageOnScroll);
     }
 
     const cleanUpContainerRef = containerRef.current;
     return () => {
-      cleanUpContainerRef.removeEventListener('scroll', handleScrollNotification);
+      cleanUpContainerRef.removeEventListener('scroll', fetchPreviousPageOnScroll);
     };
-  }, [handleScrollNotification]);
+  }, [fetchPreviousPageOnScroll]);
+
+  const scrollToTop = () => {
+    setTimeout(() => {
+      if (containerRef.current) {
+        containerRef.current.scrollTop = containerRef.current.scrollHeight;
+      }
+    }, 100);
+  };
+
+  const handleSendMessage = (message) => {
+    if (message.trim().length !== 0) {
+      createMessage({
+        author: appConfig.senderName,
+        message,
+      });
+      scrollToTop();
+    }
+  };
 
   return (
     <Box
@@ -40,11 +88,11 @@ const App = () => {
       <Box width="100%" height="100%" ref={containerRef} overflow="scroll">
         <MaxWidthContainer>
           <Box px={12} height="100%" width="100%">
-            <MessageList messages={MESSAGES} />
+            <MessageList messages={messages} />
           </Box>
         </MaxWidthContainer>
       </Box>
-      <ComposeBox />
+      <ComposeBox onSend={handleSendMessage} />
     </Box>
   );
 };
